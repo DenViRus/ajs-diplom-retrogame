03a88dff9c20cd2d0780ac4a602aaa1b23e76a00
Denis Yefremenko <denvirus83@mail.ru>
2021-12-10 04:00:10 +0400

    commit for gh-pages

--- src/js/__tests__/GameStateService.test.js
+++
@@ -1,8 +0,0 @@
-import GameStateService from "../GameStateService.js";
-
-test("load gamestate Error", () => {
-  const stateServis = new GameStateService();
-  const received = () => stateServis.load();
-  const expected = "Invalid state";
-  expect(received).toThrow(expected);
-});
